792bd318d083ba985c15372c2c90d625018f37af
Sinesipho-Sono <sinesiphobeesono@gmail.com>
2025-03-06 15:43:05 +0200

    added netlify to cors origin

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://house-of-betty.netlify.app/"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
