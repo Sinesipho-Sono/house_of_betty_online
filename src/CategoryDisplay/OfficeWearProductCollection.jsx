d804ca95a8a93bc073080f787d4a53d8b6ecbf70
Sinesipho-Sono <sinesiphobeesono@gmail.com>
2025-03-14 14:53:12 +0200

    updated Fetch API path

--- src/CategoryDisplay/OfficeWearProductCollection.jsx
+++ src/CategoryDisplay/OfficeWearProductCollection.jsx
@@ -13,7 +13,7 @@ const OfficewearProductCollection = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("https://house-of-betty-online.vercel.app/api/products")
+    fetch("/api/products")
       .then((response) => response.json())
       .then((data) => {
         setProducts(data);
